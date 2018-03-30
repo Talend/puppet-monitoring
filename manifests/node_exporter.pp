4221e40818d0cc75595af44ecfae434697eb2bce
Julien Leloup <jleloup@tlnd-jleloup.local>
2018-03-30 16:51:06 +0200

    feat(cadvisor): fix acceptance tests

--- manifests/node_exporter.pp
+++ manifests/node_exporter.pp
@@ -14,7 +14,7 @@ class monitoring::node_exporter (
     version            => $version,
     download_extension => 'tar.gz',
     real_download_url  => "https://github.com/prometheus/${exporter_name}/releases/download/v${version}/${exporter_name}-${version}.linux-amd64.tar.gz",
-    runtime_options    => "-web.listen-address=\":${port}\" \
+    runtime_options    => "--web.listen-address=:${port} \
                            --collector.textfile.directory /var/lib/node_exporter/textfile_collector \
                            --collector.systemd",
   }
