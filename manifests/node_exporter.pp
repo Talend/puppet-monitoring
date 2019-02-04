d533f03015790553e0442af62d4301d08cb130bd
Julien Leloup <jleloup@talend.com>
2019-02-04 11:31:29 +0100

    fix(Node Exporter) fix Puppet syntax :

--- manifests/node_exporter.pp
+++ manifests/node_exporter.pp
@@ -8,6 +8,7 @@ class monitoring::node_exporter (
   $service_ensure = 'running',
   $service_enable = true,
   $port = 9100,
+  $ignored_mount_points = '"^/(var/lib/docker/)|(run/docker/netns/).*"'
 ) {
 
   monitoring::daemon { $exporter_name:
@@ -15,7 +16,8 @@ class monitoring::node_exporter (
     version            => $version,
     download_extension => 'tar.gz',
     real_download_url  => "https://github.com/prometheus/${exporter_name}/releases/download/v${version}/${exporter_name}-${version}.linux-amd64.tar.gz",
-    runtime_options    => "--web.listen-address=:${port} --collector.filesystem.ignored-mount-points "^/(var/lib/docker/)|(run/docker/netns/).*"",
+    runtime_options    => "--web.listen-address=:${port}"\
+                          "--collector.filesystem.ignored-mount-points=${ignored_mount_points}",
     service_ensure     => $service_ensure,
     service_enable     => $service_enable,
   }
