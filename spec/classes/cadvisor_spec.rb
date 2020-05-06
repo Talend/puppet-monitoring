334557faa5d87d464d0fc74f9733bf57d80a3320
Olivier Tassetti <otassetti@talend.com>
2020-05-06 18:03:25 +0200

    test

--- spec/classes/cadvisor_spec.rb
+++ spec/classes/cadvisor_spec.rb
@@ -3,13 +3,8 @@ require 'spec_helper'
 describe 'monitoring::cadvisor' do
   let(:pre_condition) { 'user {docker:}' }
   let(:facts) {{ :osfamily => 'RedHat'}}
-  let(:params) do
-    {
-      :version => '0.29.0',
-      :version_checksum => 'f5c8deb31eb12cae38007f0f4a208e0b9ba2b2ad6a1c9610b32d113221880d4e',
-    }
-  end
   context 'with default values for all parameters' do
     it {should compile}
   end
 end
+
